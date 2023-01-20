5cca78fa64f33faf8b760e2fb9a0ba41f22b879c
d1ndonlymdhe <ayushshrestha8920@protonmail.com>
2023-01-20 22:18:21 +0545

    tailwind theme and database setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <div className="grid grid-cols-1 font-lato gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3B3486",
+        secondary: "#7743DB",
+        complementary: "#FFE9B1",
+        minWhite: "#FCFDF2"
+      },
+      fontFamily: {
+        lato: ["'Lato'", '"sans-serif"'],
+        montserrat: ["'Montserrat", 'sans-serif'],
+        playfair: ["'PLayfair Display", 'serif'],
+        poppins: ["'Poppins'", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 };
