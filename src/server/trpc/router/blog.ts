84dfca26dc2a4ecf5182e5cedc4a0e2e6fab50c1
d1ndonlymdhe <ayushshrestha8920@protonmail.com>
2023-01-29 19:42:46 +0545

    add blogreaction router

--- src/server/trpc/router/blog.ts
+++ src/server/trpc/router/blog.ts
@@ -23,8 +23,54 @@ export const blogRouter = router({
             code: "BAD_REQUEST",
             message: "Error"
         })
-    })
-    ,
-    comment: commentRouter
+    }),
+    comment: commentRouter,
+    reaction: publicProcedure.input(z.object({ type: z.boolean(), blogId: z.string() })).mutation(async ({ input, ctx }) => {
+        const { prisma } = ctx
+        const { type, blogId } = input
+        const token = ctx.req.cookies?.token
+        if (token) {
+            const dbToken = await prisma.token.findFirst({ where: { value: token }, include: { user: true } })
+            if (dbToken) {
+                const user = dbToken.user;
+                const blog = await prisma.blog.findFirst({ where: { id: blogId } })
+                if (blog) {
+                    //find user reaction
+                    const reactions = await prisma.blogReaction.findMany({ where: { AND: [{ blogId, userId: user.id }] } })
+                    if (reactions) {
+                        //user can have a single reaction in a blog
+                        const userReaction = reactions[0]
+                        if (userReaction) {
+                            const updatedReaction = await prisma.blogReaction.update({
+                                where: { id: userReaction.id }, data: {
+                                    ...userReaction,
+                                    type
+                                }
+                            })
+                            return {
+                                success: true,
+                                reaction: updatedReaction
+                            }
+                        }
+                    } else {
+                        const newReaction = await prisma.blogReaction.create({
+                            data: {
+                                type,
+                                blogId,
+                                userId: user.id,
+                            }
+                        })
+                        return {
+                            success: true,
+                            reaction: newReaction
+                        }
+                    }
+                }
 
+            }
+        }
+        throw new TRPCError({
+            code: "BAD_REQUEST",
+        })
+    })
 })
