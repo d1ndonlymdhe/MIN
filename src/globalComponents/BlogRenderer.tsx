63c569ec3550d077c76928204d0753110b206deb
d1ndonlymdhe <ayushshrestha8920@protonmail.com>
2023-06-23 17:27:35 +0545

    default blog render use s3

--- src/globalComponents/BlogRenderer.tsx
+++ src/globalComponents/BlogRenderer.tsx
@@ -4,6 +4,8 @@ import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
 import "@uiw/react-markdown-preview/markdown.css";
+import { getBlogImage } from "../pages/blogs/[underscoreBlogName]";
+// import getBlogImage from "../pages/api/getBlogContentImage";
 const MDPreview = dynamic(() => import("@uiw/react-markdown-preview"), { ssr: false })
 export default function BlogRenderer(props: { blog: Blog }) {
     const { blog } = props;
@@ -13,7 +15,7 @@ export default function BlogRenderer(props: { blog: Blog }) {
     return (
         <div className="grid grid-rows-[15vh_1fr_9fr] h-[85vh] w-[90vw] flex-col gap-2 overflow-auto rounded-md bg-secondary md:w-[40vw]">
             <div style={{
-                backgroundImage: `url(/api/getBlogImage?blogId=${blog.id}&authorId=${blog.authorId})`,
+                backgroundImage: `url(${getBlogImage(blog.id)})`,
                 // height: "50vh",
             }} className="h-[15vh] w-full rounded-t-md  text-black">
 
