f733aa7a4bbcba5b607699f3537d989ede5dad4f
d1ndonlymdhe <ayushshrestha8920@protonmail.com>
2023-09-17 10:05:30 +0545

    fix build errors

--- src/pages/alumni.tsx
+++ src/pages/alumni.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction, useState } from "react";
+import React, {  useState } from "react";
 // import Navbar from "../globalComponents/Navbar";
 // import AlumniImg from "../../public/images/alumni.jpg";
 // import Background from "../globalComponents/Background";
